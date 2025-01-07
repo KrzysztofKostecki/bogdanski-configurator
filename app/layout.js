d4b3d1a5920da960b40e786d139a44f25d4b60b2
Krzysztof Kostecki <krzysztof.kosteckiagh@gmail.com>
2025-01-07 10:19:35 +0100

    Add vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: 'Bogdański - Konfigurator Okien',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+       </body>
     </html>
   );
 }
